0dd1131cfd7f73fcd65a8dfa3be6fc9e12eab5d1
CGuthmann <claus.guthmann@web.de>
2022-11-29 15:25:38 -0600

    added verifier claim

--- contracts/test.sol
+++ contracts/test.sol
@@ -18,15 +18,26 @@ interface Verifier_circuit_total{
         ) external view returns (bool r);
 }
 
+interface Verifier_circuit_claim{
+    function verifyProof(
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[2] memory input
+        ) external view returns (bool r);    
+}
+
 contract testing {
 
 
     Verifier_circuit_participants verifier_circuit_participants;
     Verifier_circuit_total verifier_circuit_total;
+    Verifier_circuit_claim verifier_circuit_claim;
 
     constructor(address participant_verifier_address, address total_verifier_address, address claim_verifier_address){
         verifier_circuit_participants = Verifier_circuit_participants(participant_verifier_address);
         verifier_circuit_total = Verifier_circuit_total(total_verifier_address);
+        verifier_circuit_claim = Verifier_circuit_claim(claim_verifier_address);
     }
     function tst(
             uint[2] memory a,
@@ -49,6 +60,18 @@ contract testing {
     public view returns (bool r) {
         
         bool testBool = verifier_circuit_total.verifyProof(a,b,c,input);
+        
+        return testBool;
+    }
+
+    function test_claim(
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[2] memory input)
+    public view returns (bool r) {
+        
+        bool testBool = verifier_circuit_claim.verifyProof(a,b,c,input);
         
         return testBool;
     }
