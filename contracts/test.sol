fcaafe7922a914d1799c6a1eb0e6cb04a4655562
CGuthmann <claus.guthmann@web.de>
2022-11-29 14:19:10 -0600

    example contract

---
+++ contracts/test.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.8.17;
+
+interface Verifier_circuit_participants{
+    function verifyProof(
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[3] memory input
+        ) external view returns (bool r);
+}
+
+interface Verifier_circuit_total{
+    function verifyProof(
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[3] memory input
+        ) external view returns (bool r);
+}
+
+contract testing {
+
+
+    Verifier_circuit_participants verifier_circuit_participants;
+    Verifier_circuit_total verifier_circuit_total;
+
+    constructor(address participant_verifier_address, address total_verifier_address, address claim_verifier_address){
+        verifier_circuit_participants = Verifier_circuit_participants(participant_verifier_address);
+        verifier_circuit_total = Verifier_circuit_total(total_verifier_address);
+    }
+    function tst(
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[3] memory input)
+    public view returns (bool r) {
+        
+        bool testBool = verifier_circuit_participants.verifyProof(a,b,c,input);
+        
+        return testBool;
+    }
+
+
+    function tst_total(
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[3] memory input)
+    public view returns (bool r) {
+        
+        bool testBool = verifier_circuit_total.verifyProof(a,b,c,input);
+        
+        return testBool;
+    }
+}
