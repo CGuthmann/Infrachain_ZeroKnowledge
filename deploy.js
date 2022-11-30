f8f4d446435dae96e411acfbb51c87ebbc77f563
CGuthmann <claus.guthmann@web.de>
2022-11-29 18:53:21 -0600

    added deploy script

---
+++ deploy.js
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2020  ChainLab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const circuitName = "circuit_participant";
+
+const fs = require("fs");
+const config = require("./config.json")
+const Web3 = require("web3");
+const Contract = require("web3-eth-contract");
+const TruffleContract = require("truffle-contract");
+const provider = new Web3.providers.HttpProvider(config.node);
+const web3 = new Web3(provider);
+
+
+const account = config.account
+const delay = 15
+
+const ParticipantAddress = '';
+const TotalVerifierAddress = '';
+const ClaimVerifierAddress = '';
+const CollateralTokenAddress = '';
+const CollateralBetAddress = '';
+
+
+async function main() {
+
+	console.log(config.account);
+
+	const signer = await web3.eth.accounts.privateKeyToAccount("22aae6e36021acbf8d4a05a169d77919929d390dab212c609c319ea99c4dd298")
+	web3.eth.accounts.wallet.add(signer);
+
+	const balance = await web3.eth.getBalance(config.account);
+	console.log("balance", web3.utils.fromWei(balance, "ether"));
+
+
+    
+	let contractParticipantVerifierRawdata = fs.readFileSync('build/contracts/Verifier_circuit_participant.json');
+	let contractParticipantVerifierMetadata = JSON.parse(contractParticipantVerifierRawdata);
+	// console.log(metadata);
+
+	const contractParticipantVerifier = new web3.eth.Contract(contractParticipantVerifierMetadata.abi);
+	// console.log(contract);
+
+	const contractParticipantVerifierSend = contractParticipantVerifier.deploy({
+		data: contractParticipantVerifierMetadata.bytecode,
+		arguments: []
+	})
+
+	const contractParticipantVerifierInstance = await contractParticipantVerifierSend.send({
+		from: config.account,
+		gas: 3500000
+	})
+
+	console.log("ParticipantVerifier deployed at: ",contractParticipantVerifierInstance._address);
+
+	await new Promise(r => setTimeout(r, delay));
+
+
+
+
+
+
+
+	let contractTotalVerifierRawdata = fs.readFileSync('build/contracts/Verifier_circuit_total.json');
+	let contractTotalVerifierMetadata = JSON.parse(contractTotalVerifierRawdata);
+	// console.log(metadata);
+
+	const contractTotalVerifier = new web3.eth.Contract(contractTotalVerifierMetadata.abi);
+	// console.log(contract);
+
+	const contractTotalVerifierSend = contractTotalVerifier.deploy({
+		data: contractTotalVerifierMetadata.bytecode,
+		arguments: []
+	})
+
+	const contractTotalVerifierInstance = await contractTotalVerifierSend.send({
+		from: config.account,
+		gas: 4000005
+	})
+
+	console.log("TotalVerifier deployed at: ",contractTotalVerifierInstance._address);
+	
+
+	await new Promise(r => setTimeout(r, delay));
+
+
+
+
+
+
+	
+
+	let contractClaimVerifierRawdata = fs.readFileSync('build/contracts/Verifier_circuit_claim.json');
+	let contractClaimVerifierMetadata = JSON.parse(contractClaimVerifierRawdata);
+	// console.log(metadata);
+
+	const contractClaimVerifier = new web3.eth.Contract(contractClaimVerifierMetadata.abi);
+	// console.log(contract);
+
+	const contractClaimVerifierSend = contractClaimVerifier.deploy({
+		data: contractClaimVerifierMetadata.bytecode,
+		arguments: []
+	})
+
+	const contractClaimVerifierInstance = await contractClaimVerifierSend.send({
+		from: config.account,
+		gas: 4000005
+	})
+
+	console.log("ClaimVerifier deployed at: ",contractTotalVerifierInstance._address);
+
+
+	await new Promise(r => setTimeout(r, delay));
+
+
+
+
+	
+
+	let contractCollateralTokenRawdata = fs.readFileSync('build/contracts/CollateralToken.json');
+	let contractCollateralTokenMetadata = JSON.parse(contractCollateralTokenRawdata);
+	// console.log(metadata);
+
+	const contractCollateralToken = new web3.eth.Contract(contractCollateralTokenMetadata.abi);
+	// console.log(contract);
+
+	const contractCollateralTokenSend = contractCollateralToken.deploy({
+		data: contractCollateralTokenMetadata.bytecode,
+		arguments: []
+	})
+
+	const contractCollateralTokenInstance = await contractCollateralTokenSend.send({
+		from: config.account,
+		gas: 4000005
+	})
+
+	console.log("CollateralToken deployed at: ",contractTotalVerifierInstance._address);
+
+
+	await new Promise(r => setTimeout(r, delay));
+
+
+
+
+	//let calldata = await snarkjs.groth16.exportSolidityCallData(proof, publicSignals);
+	let contractCollateralBetRawdata = fs.readFileSync('build/contracts/CollateralBet.json');
+	let contractCollateralBetMetadata = JSON.parse(contractCollateralBetRawdata);
+	// console.log(metadata);
+
+	const contractCollateralBet = new web3.eth.Contract(contractCollateralBetMetadata.abi);
+	// console.log(contract);
+
+	const contractCollateralBetSend = contractCollateralBet.deploy({
+		data: contractCollateralBetMetadata.bytecode,
+		arguments: [contractCollateralTokenInstance._address,contractParticipantVerifierInstance._address,
+			contractTotalVerifierInstance._address,contractClaimVerifierInstance._address]
+	})
+
+	const contractCollateralBetInstance = await contractCollateralBetSend.send({
+		from: config.account,
+		gas: 3500000
+	})
+    console.log("MainVerifier deployed at: ",contractCollateralBetInstance._address);
+	
+}
+
+main();
